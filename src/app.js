3ee568b768fa3ddaf52e5912c50b743863abd9c0
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-05 14:17:41 +0100

    working create, find and delete, faulty update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 const yargs = require("yargs");
 const { sequelize } = require("./db/connection");
+const { addMovie, listMovies, updateMovie, deleteMovie } = require("./movie/movieMethods");
+const Movie = require("./movie/movieTable");
 // imports for crud functions
 
-const app = async = (yargsObj) => {
+const app = async (yargsObj) => {
     try {
+        await sequelize.sync();
         if (yargsObj.add) {
             // add movie to database
+            await addMovie({title: yargsObj.title, actor: yargsObj.actor});
+            console.log(`Success, added ${yargsObj.title} to the db. `);
         } else if (yargsObj.list) {
             //list all movies
+            console.log(await listMovies());
+            // console.log("List function completed.");
         } else if (yargsObj.update){
-            // update movie
+            //state new/update first 
+            await updateMovie({newtitle: yargsObj.newtitle}, {title: yargsObj.title});
+              //log the changes
         } else if (yargsObj.delete){
             // delete all movies
+            await deleteMovie({title: yargsObj.title});
+            console.log(`Success, deleted ${yargsObj.title}`);
         } else {
             // do something
             console.log("Incorrect command");
@@ -23,3 +34,12 @@ const app = async = (yargsObj) => {
 };
 
 app(yargs.argv);
+
+// ================node commands======================
+// USING create = node src/app.js --add --title="SpiderMan" --actor="Tom Holland"
+// USING destroy = node src/app.js --delete --title="SpiderMan"
+// USING findALL = node src/app.js --list
+
+// update working out
+// USING fixed variables = node src/app.js --update --newTitle="Kung Fu Panda Updated" --title="Kung Fu Panda" 
+// USING one fixed, one variable = node src/app.js --update --newtitle="AntMan Updated" --title="AntMan"
---
+++ src/movie/movieMethods.js
@@ -0,0 +1,58 @@
+const { sequelize } = require("../db/connection");
+const Movie = require("./movieTable");
+
+// add movie function
+exports.addMovie = async (movieObj) => {
+    //create one entry in the db
+    try {
+        //await model create method 
+        await Movie.create(movieObj);
+        // log the intermmittent message
+        console.log("Success, creating new entry");
+    } catch (error) {
+        console.log(error)
+    }
+};
+// list one movie function
+exports.listMovies = async () => {
+    // find One movie object from db 
+    try {
+        return await Movie.findAll();
+        } catch (error) {
+        console.log(error);
+    }
+};
+
+// update movie function by title
+exports.updateMovie = async (movieObj) => {
+    try {
+        //await model update one method
+        //state new update first, current update last
+        await Movie.update({ 
+            title: "BatMan Updated" }, 
+        { where: { 
+            title: movieObj.title 
+        } 
+    });
+        console.log("Updated movie in DB.");
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+//delete movie function usign title
+exports.deleteMovie = async (movieObj) => {
+    //delete One movie from the db
+    try {
+        await Movie.destroy({
+            //select current title 
+            where: {
+                title: movieObj.title 
+            }
+            });
+            //log output
+            console.log("Deleted Movie from DB.")
+    } catch (error) {
+        console.log(error)
+    }
+};
